fd05cb739048815143dc6551848456a784fd62fa
JhonCode <jjv20618@gmail.com>
2023-05-30 19:49:36 -0600

    Delete NestPlayerWeb directory

--- NestPlayerWeb/Distribuciones/DescargarWindows.php
+++
@@ -1,9 +0,0 @@
-<?php phpinfo();
-?>
-
-<?php
-$ruta = "Distribuciones/Windows/NestPlayerWindows-Portable-x64.rar";
-header("Content-disposition: attachment; filename=NestPlayerWindows-Portable-x64.rar");
-header("Content-type: application/x-rar-compressed");
-readfile($ruta);
-?>
